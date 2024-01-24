b7d3f725c759e438ad7137d4d64cac8971cd875a
canslercp <canslercp@gmail.com>
2024-01-24 09:37:13 -0500

    started on the views and models

--- training/weightlifting/models.py
+++ training/weightlifting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=200)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+class SurveyResponse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    focus = models.CharField(max_length=40)
+    competition = models.BooleanField()
+    startDate = models.DateField()
+    duration = models.IntegerField()
+    emphasis = models.CharField(max_length=100)
