45861ed6c465e9d95d037a849b62f6c59f4a8d5f
canslercp <canslercp@gmail.com>
2024-02-02 13:51:44 -0500

    updated admin site

--- training/weightlifting/admin.py
+++ training/weightlifting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, SurveyResponse, Competition
+
+admin.site.register(User)
+admin.site.register(SurveyResponse)
+admin.site.register(Competition)
