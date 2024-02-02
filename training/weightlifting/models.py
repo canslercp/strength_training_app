85948b289c95507592f7b4c4afe7275ec62a83eb
canslercp <canslercp@gmail.com>
2024-02-02 12:39:01 -0500

    added migrations

--- training/weightlifting/models.py
+++ training/weightlifting/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
     email = models.EmailField(max_length=200)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
 
 class SurveyResponse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +16,8 @@ class SurveyResponse(models.Model):
     startDate = models.DateField()
     duration = models.IntegerField()
     emphasis = models.CharField(max_length=100)
+
+class Competition(models.Model):
+    startDate = models.DateField()
+    priority = models.IntegerField()
+    name = models.CharField(max_length=40)
