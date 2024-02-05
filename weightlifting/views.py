c317442b272338f20d095c4880db3d9751492fe9
canslercp <canslercp@gmail.com>
2024-02-05 11:47:29 -0500

    added routes

--- weightlifting/views.py
+++ weightlifting/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# from .models import program
+# from .serializers import ProgramSerializer
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the weightlifting index.")
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {
+            'Endpoint': '/programs/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of programs'
+        },
+        {
+            'Endpoint': '/programs/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single program object'
+        },
+        {
+            'Endpoint': '/programs/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Cretes new program with data sent in post request'
+        },
+        {
+            'Endpoint': '/programs/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing program with data sent in post'
+        },
+        {
+            'Endpoint': '/programs/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an existing program'
+        }
+    ]
+    return Response(routes)
